e8fedc0abe3291c5b9282551f05c919c6dbd0a41
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-10 17:15:47 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,11 +2,7 @@ module Players
   class Computer < Player
 
     def move(input)
-      puts "Please enter 1-9:"
-      i = gets.chomp
-      if valid_move?(i)
-        return i
-      end
+
     end
   end
 end
